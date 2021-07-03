f177914452cd8bf62dbc4b781a00983a5ad1fc0a
Chris Turner <chris@forloop.space>
2021-07-03 06:30:24 +0100

    fix: Add a small delay between action

--- src/main/java/io/skyshard/services/AttackServiceImpl.java
+++ src/main/java/io/skyshard/services/AttackServiceImpl.java
@@ -3,11 +3,13 @@ package io.skyshard.services;
 import io.skyshard.domain.Target;
 import io.skyshard.utils.MathUtils;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.awt.*;
 import java.awt.event.KeyEvent;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @Service
@@ -22,12 +24,19 @@ public class AttackServiceImpl implements AttackService {
      * the target, press the '1' keyboard button.
      */
     @Override
+    @SneakyThrows
     public void attack(final Target target) {
 
         robot.mouseMove(target.x() - 5, target.y() - 5);
         robot.keyPress(KeyEvent.VK_1);
         robot.delay(MathUtils.random(25, 50));
         robot.keyRelease(KeyEvent.VK_1);
+
+        // Move the mouse out the way.
+        robot.mouseMove(-1, -1);
+
+        // Wait just a second before going again
+        TimeUnit.MILLISECONDS.sleep(MathUtils.random(350, 750));
     }
 
 }
